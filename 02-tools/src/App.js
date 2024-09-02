53fad171b46bb372726ad5c5705dcc5e88f23bf6
Brian Holt <btholt@gmail.com>
2024-09-02 13:13:25 -0500

    tools

--- 02-tools/src/App.js
+++ 02-tools/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -24,5 +27,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
