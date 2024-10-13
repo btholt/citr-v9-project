95be8f1f54c6fba0203fb0b386a005dccdeabd90
Brian Holt <btholt@gmail.com>
2024-10-13 16:00:37 -0500

    tanstack routing

---
+++ 09-routing/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+const App = () => {
+  return (
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ 09-routing/src/routes/__root.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import PizzaOfTheDay from "../PizzaOfTheDay";
+import Header from "../Header";
+import { CartContext } from "../contexts";
+
+export const Route = createRootRoute({
+  component: () => {
+    const cartHook = useState([]);
+    return (
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
+});
