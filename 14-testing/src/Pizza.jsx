cc972adfe63ee1571f9a591adbdef7145e88ed10
Brian Holt <btholt@gmail.com>
2024-10-16 23:30:57 -0500

    more tests

--- 14-testing/src/Pizza.jsx
+++ 14-testing/src/Pizza.jsx
@@ -3,7 +3,10 @@ const Pizza = (props) => {
     <div className="pizza">
       <h1>{props.name}</h1>
       <p>{props.description}</p>
-      <img src={props.image} alt={props.name} />
+      <img
+        src={props.image ? props.image : "https://picsum.photos/200"}
+        alt={props.name}
+      />
     </div>
   );
 };
--- 14-testing/vite.config.js
+++ 14-testing/vite.config.js
@@ -2,13 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // TODO: this is to make ecosystem work, remove when fixed
-    // fs: {
-    //   strict: false,
-    // },
     proxy: {
       "/api": {
         target: "http://localhost:3000",
@@ -22,11 +17,12 @@ export default defineConfig({
   },
   plugins: [TanStackRouterVite(), react()],
   test: {
-    setupFiles: ["vitest-browser-react"],
-    browser: {
-      enabled: true,
-      name: "firefox",
-      provider: "playwright",
-    },
+    // setupFiles: ["vitest-browser-react"],
+    // browser: {
+    //   enabled: true,
+    //   name: "firefox",
+    //   provider: "playwright",
+    // },
+    environment: "happy-dom",
   },
 });
